52575c20c9a7f2cb0624b69c80ce8fdc40f66b8b
fixen94 <fixen94@gmail.com>
2019-10-15 10:09:39 +1000

    more tests

--- test/nmax_test.rb
+++ test/nmax_test.rb
@@ -9,7 +9,21 @@ class NmaxTest < Minitest::Test
     refute_nil ::Nmax::VERSION
   end
 
-  def test_that_it_works
+  def test_returns_array
     assert_equal(Nmax.run(5).class, Array)
+  end
+
+  def test_returns_nil_without_parameter
+    assert_nil(Nmax.run(''))
+  end
+
+  def test_no_duplicates
+    result = Nmax.run(30)
+    assert_equal(result, result.uniq)
+  end
+
+  def test_only_numbers
+    result = Nmax.run(30)
+    assert(result.all? { |i| i.is_a? Integer })
   end
 end
